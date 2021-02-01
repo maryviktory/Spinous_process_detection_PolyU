a781de1903dfdf62feb48ebcb4625d6bae36d49c
Maria Victorova <maryviktory@gmail.com>
2021-02-01 08:50:23 +0100

    - PolyU data. FCN test outputs videos with detected point and respected label. At the end the trajectory is generated

---
+++ sp_utils/__init__.py
@@ -0,0 +1,6 @@
+# from sp_utils.imfusion_utils import *
+from sp_utils.functions import *
+from sp_utils.config import *
+from sp_utils.Loader_dataset import *
+from sp_utils.model_pose_resnet import *
+from sp_utils.f_processing import *
